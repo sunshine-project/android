5b19ff4cf163865ca3ec9baf32e4e5495b119b76
yujin <jini1514@soongsil.ac.kr>
2024-02-06 12:05:53 +0900

    setting: 폰트 세팅

--- app/src/main/java/com/sunshine/android/ui/theme/Type.kt
+++ app/src/main/java/com/sunshine/android/ui/theme/Type.kt
@@ -2,18 +2,60 @@ package com.sunshine.android.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.sunshine.android.R
 
 // Set of Material typography styles to start with
+val galmuri = FontFamily(
+    Font(R.font.galmuri7, FontWeight.Normal, FontStyle.Normal),
+    Font(R.font.galmuri11_bold, FontWeight.Bold, FontStyle.Normal)
+
+)
+
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+    titleLarge  = TextStyle(
+        fontFamily = galmuri,
+        fontWeight = FontWeight.Normal,
+        fontSize = 24.sp,
+    ),
+    titleMedium = TextStyle(
+        fontFamily = galmuri,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp,
+    ),
+    titleSmall= TextStyle(
+        fontFamily = galmuri,
+        fontWeight = FontWeight.Normal,
+        fontSize = 20.sp,
+    ),
+    bodyLarge= TextStyle(
+        fontFamily = galmuri,
+        fontWeight = FontWeight.Bold,
+        fontSize = 16.sp,
+    ),
+    bodyMedium= TextStyle(
+        fontFamily = galmuri,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
+    ),
+    labelLarge = TextStyle(
+        fontFamily = galmuri,
+        fontWeight = FontWeight.Bold,
+        fontSize = 14.sp,
+    ),
+    labelMedium = TextStyle(
+        fontFamily = galmuri,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+    ),
+    labelSmall = TextStyle(
+        fontFamily = galmuri,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp,
     )
     /* Other default text styles to override
     titleLarge = TextStyle(
