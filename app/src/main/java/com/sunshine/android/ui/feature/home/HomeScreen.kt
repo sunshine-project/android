0aab7b0c3a098b753ca9696a9fbb2caf7f72299c
roian6 <roian6@naver.com>
2024-02-13 21:31:05 +0900

    chore: move UserInfo.kt

--- app/src/main/java/com/sunshine/android/ui/feature/home/HomeScreen.kt
+++ app/src/main/java/com/sunshine/android/ui/feature/home/HomeScreen.kt
@@ -23,7 +23,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.sunshine.android.R
-import com.sunshine.android.data.UserInfo
+import com.sunshine.android.data.model.UserInfo
 import com.sunshine.android.ui.theme.Brown
 import com.sunshine.android.ui.theme.Red
 import com.sunshine.android.ui.theme.Yellow
